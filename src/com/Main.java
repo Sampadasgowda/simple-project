6c4af0774033204e6cef0cce7e7e4d57b4cb3f9c
Sampadasgowda <sampadagowda18@gmail.com>
2025-03-11 11:23:50 +0530

    first commit

---
+++ src/com/Main.java
@@ -0,0 +1,15 @@
+package com;
+
+import com.model.Book;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Book book=new Book();
+		book.setTitle("Java");
+		book.setAuthor("Herbert");
+		System.out.println(book);
+	}
+
+}
+ 
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author 91843
+ *
+ */
+module simple_project {
+}
